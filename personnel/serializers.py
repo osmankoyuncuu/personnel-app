3fefcb788f9fd4f2017e6b0cddc0cc8523de3565
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-10 23:08:12 +0300

    add serializer

---
+++ personnel/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Department, Personnel
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ("name",)
+        
+        
+class PersonnelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Personnel
+        fields = "__all__"
