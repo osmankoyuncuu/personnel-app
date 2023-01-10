5ed1c6ef1f1e31535cb8c9a0176174cd4bdd69cf
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-10 22:22:53 +0300

    deparment and personnel models created

--- personnel/models.py
+++ personnel/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Personnel(models.Model):
+    TITLE = (
+        (1, 'Team Lead'),  # db, kullanıcı
+        (2, 'Mid Lead'),
+        (3, 'Junior')
+    )
+    GENDER = (
+        (1, 'Female'),  # db, kullanıcı
+        (2, 'Male'),
+        (3, 'Other'),
+        (4, 'Prefer Not Say'),
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_staffed = models.BooleanField(default=False)
+    title = models.SmallIntegerField(choices=TITLE, default=3)
+    gender = models.SmallIntegerField(choices=GENDER, default=4)
+    salary = models.IntegerField(blank=True, null=True)
+    start_date = models.DateTimeField(auto_now_add=True)
+    create_user = models.DateTimeField(auto_now_add=True)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.department}"
