4ec12e14da395f687f5340e384b2cdfd4582d3da
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-10 22:33:30 +0300

    add djangorestframework

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party
+    'rest_framework',
     #my app
     'personnel',
 ]
