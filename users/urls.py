7f839c768a1b8b7409b0a324cc7a240e78e2492e
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-11 22:49:30 +0300

    add register and signal

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
-#from .views import RegisterAPI
+from .views import RegisterAPI
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
-    #path("register/", RegisterAPI.as_view())
+    path("register/", RegisterAPI.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+#from rest_framework import status
+#from rest_framework.response import Response
+#from rest_framework.authtoken.models import Token
 
-# Create your views here.
+class RegisterAPI(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
