3465651f04b95d5cb507b5c7d624147c51c80e9a
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-12 01:31:10 +0300

    add permissions

--- personnel/serializers.py
+++ personnel/serializers.py
@@ -4,10 +4,18 @@ from .models import Department, Personnel
 class PersonnelSerializer(serializers.ModelSerializer):
     department = serializers.StringRelatedField()
     department_id = serializers.IntegerField(write_only=True)
+    start_date = serializers.DateTimeField(write_only=True, required=False)
+    days_since_joined = serializers.SerializerMethodField()
+    
     class Meta:
         model = Personnel
-        fields = ("id","create_user", "first_name", "last_name", "is_staffed", "title", "gender", "salary", "start_date", "department", "department_id")
-
+        fields = ("id", "days_since_joined","create_user", "first_name", "last_name", "is_staffed", "title", "gender", "salary", "start_date", "department", "department_id" )
+        
+    def get_days_since_joined(self, obj):
+        import datetime
+        current_time = datetime.datetime.now()
+        return current_time.day - obj.start_date.day
+    
 class DepartmentSerializer(serializers.ModelSerializer):
     personal_count = serializers.SerializerMethodField()
     class Meta:
@@ -16,6 +24,7 @@ class DepartmentSerializer(serializers.ModelSerializer):
         
     def get_personal_count(self, obj):
         return obj.personals.count()
+        
 class DepartmentPersonalSerializer(serializers.ModelSerializer):
     personals = PersonnelSerializer(many=True, required=False)
     personal_count = serializers.SerializerMethodField()
--- personnel/views.py
+++ personnel/views.py
@@ -2,15 +2,19 @@ from rest_framework import  viewsets
 from rest_framework.generics import ListAPIView
 from .models import Department, Personnel
 from .serializers import DepartmentSerializer, DepartmentPersonalSerializer, PersonnelSerializer 
+from .permissions import IsStaffOrReadOnly
 
 class DepartmentView(ListAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = (IsStaffOrReadOnly,)
     
 class DepartmentPersonalView(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentPersonalSerializer
+    permission_classes = (IsStaffOrReadOnly,)
     
 class PersonnelView(viewsets.ModelViewSet):
     queryset = Personnel.objects.all()
     serializer_class = PersonnelSerializer
+    permission_classes = (IsStaffOrReadOnly,)
