821df6c5727075854e7135bc4cb5fb8e74fdd251
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-11 22:58:51 +0300

    add custom token serializer

--- main/settings.py
+++ main/settings.py
@@ -137,3 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+}
--- users/serializers.py
+++ users/serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-
-#from dj_rest_auth.serializers import TokenSerializer
+from dj_rest_auth.serializers import TokenSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
 
@@ -49,3 +48,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+class UserTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
+    
+    
+class CustomTokenSerializer(TokenSerializer):
+    user = UserTokenSerializer(read_only = True)
+    
+    class Meta(TokenSerializer.Meta):
+        fields = ("key", "user")
